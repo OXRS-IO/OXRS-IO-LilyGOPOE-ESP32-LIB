698ebbc9a867af2ca1368d4cdd6967c8d237b9cf
ben <ben.jones12@gmail.com>
2023-07-14 20:03:28 +1200

    add home asssistant discovery config helpers

--- src/OXRS_LILYGOPOE.h
+++ src/OXRS_LILYGOPOE.h
@@ -50,6 +50,11 @@ class OXRS_LILYGOPOE : public Print
     boolean publishStatus(JsonVariant json);
     boolean publishTelemetry(JsonVariant json);
 
+    // Helpers for Home Assistant discovery
+    bool isHassDiscoveryEnabled();
+    void getHassDiscoveryJson(JsonVariant json, char * id, char * name);
+    bool publishHassDiscovery(JsonVariant json, char * component, char * id);
+
     // Implement Print.h wrapper
     virtual size_t write(uint8_t);
     using Print::write;
